7ee9948a375fbdbd099f66132338821f936fc9bc
Ramin Mohammadi <rammoh5346@gmail.com>
2022-07-16 14:06:05 -0500

    setting up play game screen

--- navigation/screens/PlayGame.tsx
+++ navigation/screens/PlayGame.tsx
@@ -4,7 +4,17 @@ import { Text, View, Button, StyleSheet } from "react-native";
 function PlayGameScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Play Game</Text>
+      <View style={styles.menuButton}>
+          <Button title="Menu"  />
+      </View>
+      
+      <Text>
+        Score: 
+      </Text>
+
+      <View style={styles.picButton}>
+          <Button title="Take Picture"  />
+      </View>
     </View>
   );
 }
@@ -17,4 +27,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  picButton: {
+    width: 150,
+    height: 40,
+    backgroundColor: "orange",
+  },
+  menuButton: {
+    width: 150,
+    height: 40,
+    backgroundColor: "black",
+  }
 });
